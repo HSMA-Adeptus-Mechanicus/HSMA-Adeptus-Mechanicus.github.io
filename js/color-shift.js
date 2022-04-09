7890150fdc5d0b6a1dcdbf894cb19d6ea64298c7
Jan Zimmermann <Janzoo1234@gmail.com>
2022-04-09 19:18:15 +0200

    added onmouseover and onmouseout events

--- js/color-shift.js
+++ js/color-shift.js
@@ -2,6 +2,26 @@ import { mulberry32 } from "./mulberry.js";
 
 window.addEventListener("load", () =>
 {
+	for (let elem of document.querySelectorAll(".team > div")) {
+
+		elem.onmouseover = function () {
+			elem.style.removeProperty("background-color");
+			let color = getComputedStyle(elem).backgroundColor;
+			let values = color.match(/(\d+).*?(\d+).*?(\d+)/).slice(1);
+			let brighten = 1.2;
+			values = values.map(v => Math.round(v * brighten));
+			elem.style.backgroundColor = `rgb(${values.join(", ")})`;
+		}
+		elem.onmouseout = function () {
+			elem.style.removeProperty("background-color");
+			let color = getComputedStyle(elem).backgroundColor;
+			let values = color.match(/(\d+).*?(\d+).*?(\d+)/).slice(1);
+			let brighten = 1.0;
+			values = values.map(v => Math.round(v * brighten));
+			elem.style.backgroundColor = `rgb(${values.join(", ")})`;
+		}
+	}
+
 	shiftColors();
 	let observer = new MutationObserver(() =>
 	{
@@ -18,10 +38,8 @@ function shiftColors()
 		elem.style.removeProperty("background-color");
 		let color = getComputedStyle(elem).backgroundColor;
 		let values = color.match(/(\d+).*?(\d+).*?(\d+)/).slice(1);
-		// let brighten = rand.randRange(0.6, 1.1);
-		let brighten = rand.randRange(0.8, 1.2);
-
-		values = values.map(v => Math.round(v));
+		let brighten = 1.0;
+		values = values.map(v => Math.round(v * brighten));
 		elem.style.backgroundColor = `rgb(${values.join(", ")})`;
 	}
 }
